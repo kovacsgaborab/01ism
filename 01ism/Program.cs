82f3594a0d18e82a3066fc9159dc68066cf38c8b
kovacsgaborab <kovacsgaborab@gmail.com>
2020-09-08 10:53:05 +0200

    Dokumentáció megírása 1.0

--- 01ism/Program.cs
+++ 01ism/Program.cs
@@ -6,7 +6,16 @@ namespace _01ism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");      // \n - new line, \t - tabulátor
+                                                    // \ jelek közé írva illeszthetünk be különleges karaktereket
+
+            double tortjellegu = 10 / 3.0;          //egész szám osztva egész számmal egész számot ad vissza
+            Console.WriteLine(tortjellegu);         //racionális szám osztva egésszel racionálist ad
+
+            tortjellegu = 10 % 3.0;                 // % jellel maradékot kapunk vissza
+            Console.WriteLine(tortjellegu);
+
+            Console.ReadKey();
         }
     }
 }
